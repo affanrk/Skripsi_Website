6eb105047af8eb638cc6944cc86373f655fc634a
affanrk <affan.kurniadi@gmail.com>
2024-11-13 07:30:29 +0700

    api flask address

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -49,7 +49,7 @@
             var formData = new FormData();
             formData.append('image', imageFile);
 
-            fetch('https://api-gymformx.vercel.app/predict', {
+            fetch('https://http://127.0.0.1:8080/predict', {
                     method: 'POST',
                     body: formData
                 })
